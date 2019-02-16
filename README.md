b36ff4096217e0579790aa2891d2bc89c054961b
huguobo <huguobo@meituan.com>
2019-02-16 18:02:15 +0800

    gulpfile

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # myResume
-  胡国博的个人在线简历，求认领
-  详细情况请看简历内容
-  [胡国博的在线简历](http://huguobo.github.io/myResume/)
+
+Welcome!
+
+[Cooper's Online CV](http://huguobo.github.io/myResume/)
--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,51 @@
 var gulp = require('gulp');
-
+var babel = require('gulp-babel'); // 语法转换
 var compass = require('gulp-compass');
+var minifyCSS = require('gulp-minify-css');
+var rename = require('gulp-rename');//重命名
+var uglify = require('gulp-uglify');//js压缩
+var concat = require('gulp-concat'); // 合并
+var sass = require('gulp-sass') // scc编译
+var del = require('del');
 
-gulp.task('compass', function() {
-    gulp.src('./scss/**')
-        .pipe(compass({
-            comments: false,
-            css: 'css',
-            sass: 'scss',
-            image: 'img'
-        }))
-        .pipe(gulp.dest('css'));
+// js 
+gulp.task('js', async () => {
+    await gulp.src('./src/js/*.js')
+        .pipe(uglify())
+        .pipe(rename({ extname: ".min.js"}))
+        .pipe(gulp.dest('dist/js'))
 });
 
-gulp.task('vendor', function() {
-    gulp.src(['./bower_components/pagepiling.js/jquery.pagepiling.min.js',
-            './bower_components/pagepiling.js/jquery.pagepiling.css'])
-        .pipe(gulp.dest('./vendor'));
+//scss
+gulp.task("scss", async () => {
+    await gulp.src('./src/scss/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(concat('main.css'))
+        .pipe(minifyCSS())
+        .pipe(rename({ extname: ".min.css" }))
+        .pipe(gulp.dest('dist/css'))
 });
 
-gulp.task('default', function() {
-    gulp.run('compass');
-    gulp.run('vendor');
-
-    gulp.watch('./scss/**', function(event) {
-        gulp.run('compass');
-    });
+//css
+gulp.task("css", async () => {
+    await gulp.src('./src/css/*.css')
+        .pipe(concat('vendor.css'))
+        .pipe(minifyCSS())
+        .pipe(rename({ extname: ".min.css" }))
+        .pipe(gulp.dest('dist/css'))
 });
+
+// clean
+gulp.task('clean', async ()=>{
+    await del(['dist/*']);
+});
+
+//watch
+gulp.task('watch', async ()=>{
+    gulp.watch('./src/scss/*.scss', gulp.series('scss'))
+    gulp.watch('./src/css/*.css', gulp.series('css'))
+    gulp.watch('./src/js/*.js', gulp.series('js'))
+})
+
+//gulp.series是串行 gulp.parallel是并行
+gulp.task('default',gulp.series('clean',gulp.parallel('js','css','scss')),'watch');
